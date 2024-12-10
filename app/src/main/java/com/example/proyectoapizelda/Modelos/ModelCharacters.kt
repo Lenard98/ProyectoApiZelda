3c04a95fac87f835913a23d3da3af7f29af2dd8b
Lenard98 <lenardsalinas5@gmail.com>
2024-12-10 16:00:37 -0600

    Codificacion ModelCharacters

--- app/src/main/java/com/example/proyectoapizelda/Modelos/ModelCharacters.kt
+++ app/src/main/java/com/example/proyectoapizelda/Modelos/ModelCharacters.kt
@@ -1,2 +1,17 @@
 package com.example.proyectoapizelda.Modelos
 
+data class CharacterResponse(
+    val success: Boolean,
+    val count: Int,
+    val data: List<Character>
+)
+
+data class Character(
+    val id: String,
+    val name: String,
+    val description: String,
+    val gender: String?,
+    val race: String?,
+    val appearances: List<String>,
+    val imageUrl: String?
+)
