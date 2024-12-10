c72b33fd586bb3a072cc79016c505d56a64c734b
omarb <alebanepaz@gmail.com>
2024-12-10 15:48:10 -0600

    Codificacion de Retrofit

--- app/src/main/java/com/example/proyectoapizelda/implementacionAPI/RetroFitConfig.kt
+++ app/src/main/java/com/example/proyectoapizelda/implementacionAPI/RetroFitConfig.kt
@@ -1,2 +1,15 @@
 package com.example.proyectoapizelda.implementacionAPI
 
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+
+object RetroFitConfig {
+    private const val BASE_URL = "https://zelda.fanapis.com/api/"
+
+    val retrofit: Retrofit by lazy {
+        Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .addConverterFactory(GsonConverterFactory.create())
+            .build()
+    }
+}
