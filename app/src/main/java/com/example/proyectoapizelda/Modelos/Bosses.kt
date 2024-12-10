772a0ea412bfbaf9d0cffd9c30648bc28faaf258
DarwinZCC <dzcc2003@gmail.com>
2024-12-10 16:02:42 -0600

    Codificación de Modelo de Bosses.

--- app/src/main/java/com/example/proyectoapizelda/Modelos/Bosses.kt
+++ app/src/main/java/com/example/proyectoapizelda/Modelos/Bosses.kt
@@ -1,2 +1,19 @@
 package com.example.proyectoapizelda.Modelos
 
+data class BossesResponse(
+    val success: Boolean,
+    val count: Int,
+    val data: List<Boss>
+)
+
+
+
+data class Boss(
+    val name: String,
+    val description: String,
+    val appearances: List<String>,
+    val gender: String? = null,
+    val race: String? = null,
+    val id: String
+)
+
