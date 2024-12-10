4eb5016206a8d38b957817e71c8070645998c979
Franciuuus <160555089+Franciuuus@users.noreply.github.com>
2024-12-10 15:56:43 -0600

    Codificacion ModelDungeons

--- app/src/main/java/com/example/proyectoapizelda/Modelos/ModelDungeons.kt
+++ app/src/main/java/com/example/proyectoapizelda/Modelos/ModelDungeons.kt
@@ -1,2 +1,14 @@
 package com.example.proyectoapizelda.Modelos
 
+data class Dungeon(
+    val id: String,
+    val name: String,
+    val description: String,
+    val appearances: List<String>
+)
+
+data class DungeonsResponse(
+    val success: Boolean,
+    val count: Int,
+    val data: List<Dungeon>
+)
