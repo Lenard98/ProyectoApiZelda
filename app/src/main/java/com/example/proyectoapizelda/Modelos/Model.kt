4be0ce4c4620e08f0febd7802af86f75f104aac1
omarb <alebanepaz@gmail.com>
2024-12-10 16:09:29 -0600

    Codificacion de Model

--- app/src/main/java/com/example/proyectoapizelda/Modelos/Model.kt
+++ app/src/main/java/com/example/proyectoapizelda/Modelos/Model.kt
@@ -1,2 +1,17 @@
 package com.example.proyectoapizelda.Modelos
 
+data class GameResponse(
+    val success: Boolean,
+    val count: Int,
+    val data: List<Game>
+)
+
+data class Game(
+    val id: String,
+    val name: String,
+    val description: String,
+    val developer: String,
+    val publisher: String,
+    val released_date: String
+)
+
