1272d32dbecc4532de312eca8ec2b4f2a11e5a60
libnyvr <163462086+libnyvr@users.noreply.github.com>
2024-12-10 17:18:22 -0600

    Codificacion HomeScreen

--- app/src/main/java/com/example/proyectoapizelda/Home.kt
+++ app/src/main/java/com/example/proyectoapizelda/Home.kt
@@ -1,2 +1,49 @@
 package com.example.proyectoapizelda
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.unit.dp
+import coil3.compose.AsyncImage
+
+
+
+@Composable
+fun HomeScreen() {
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0, 0, 0))
+    ) {
+
+        AsyncImage(
+            model = "https://wallpapers.com/images/featured-full/triforce-got6xicxxskxgye3.jpg",
+            contentDescription = null,
+            modifier = Modifier.fillMaxSize(),
+            alignment = Alignment.Center,
+        )
+
+        Text(
+            text = "APIZELDA",
+            style = MaterialTheme.typography.headlineLarge.copy(color = Color.White),
+            modifier = Modifier
+                .align(Alignment.TopCenter)
+                .padding(top = 32.dp)
+        )
+
+        Text(
+            text = "®ADFLL",
+            style = MaterialTheme.typography.bodyLarge.copy(color = Color.White),
+            modifier = Modifier
+                .align(Alignment.BottomEnd)
+                .padding(16.dp)
+            )
+        }
+}
